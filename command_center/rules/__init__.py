218dbfd57a2a281eb10e12be47fe78e38726116d
userf8a2c4 <250608616+userf8a2c4@users.noreply.github.com>
2026-02-06 13:03:15 -0600

    Export new command center rule checks

--- command_center/rules/__init__.py
+++ command_center/rules/__init__.py
@@ -3,11 +3,21 @@
 Individual detection rules for the command center.
 """
 
+from .benford_first_digit import check_benford_first_digit
 from .benford_second_digit import benford_second_digit_test, extract_second_digit
+from .consistencia_agregada import check_consistencia_agregada
+from .correlacion_departamental import check_correlacion_departamental
 from .last_digit_uniformity import last_digit_uniformity_test
+from .nulos_anomalias import check_nulos_anomalias
 from .spike_time_series import detect_spike_in_time_series
+from .turnout_zscore import check_turnout_zscore
 
 __all__ = [
+    "check_benford_first_digit",
+    "check_consistencia_agregada",
+    "check_correlacion_departamental",
+    "check_nulos_anomalias",
+    "check_turnout_zscore",
     "benford_second_digit_test",
     "detect_spike_in_time_series",
     "extract_second_digit",
